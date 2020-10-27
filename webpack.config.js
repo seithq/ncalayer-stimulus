9fbfd6329418164a0146e67c66a1d40b84b9c6a1
danikarik <daniyar@hey.com>
2020-10-27 23:47:28 +0600

    Change output to static

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,40 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: {
-    bundle: "./src/index.js"
+    bundle: "./src/index.js",
   },
 
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "public")
+    path: path.resolve(__dirname, "dist"),
   },
 
   mode: "production",
   devtool: "source-map",
 
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+  },
+
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: [
-          /node_modules/
-        ],
-        use: [
-          { loader: "babel-loader" }
-        ]
-      }
-    ]
-  }
+        exclude: [/node_modules/],
+        use: [{ loader: "babel-loader" }],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "NCALayer Stimulus Demo",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
 }
