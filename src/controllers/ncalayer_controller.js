9aa14796a1a41311b4e8dbb19d5fd96566ed353d
danikarik <daniyar@hey.com>
2020-10-29 15:51:14 +0600

    Add locale, not-before and not-after boxes

--- src/controllers/ncalayer_controller.js
+++ src/controllers/ncalayer_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 import Client, { extractKeyAlias } from "@seithq/ncalayer"
 
 export default class extends Controller {
-  static targets = ["error", "ui", "version", "path", "password", "key-list"]
+  static targets = ["error", "ui", "version", "path", "password", "key-list", "not-before"]
 
   initialize() {}
 
@@ -60,19 +60,33 @@ export default class extends Controller {
     this.targets.find("error").classList.add("hidden")
   }
 
-  chooseKeyType(e) {
-    this.data.set("keytype", e.target.getAttribute("keytype"))
+  markAsRadioButton(element, isActive) {
+    if (isActive) {
+      element.classList.add("bg-teal-700", "text-white")
+      element.classList.remove("bg-white", "text-teal-700")
+    } else {
+      element.classList.add("bg-white", "text-teal-700")
+      element.classList.remove("bg-teal-700", "text-white")
+    }
+  }
 
+  chooseKeyType(e) {
+    this.data.set("keytype", e.target.dataset.keytype)
     const keyTypes = [...e.target.parentNode.getElementsByTagName("div")]
     keyTypes.forEach((element) => {
-      if (element.getAttribute("keytype") === this.data.get("keytype")) {
-        element.classList.add("bg-teal-700", "text-white")
-        element.classList.remove("bg-white", "text-teal-700")
-      } else {
-        element.classList.add("bg-white", "text-teal-700")
-        element.classList.remove("bg-teal-700", "text-white")
-      }
+      this.markAsRadioButton(element, element.dataset.keytype === this.data.get("keytype"))
     })
+  }
+
+  setLocale(e) {
+    this.data.set("locale", e.target.dataset.locale)
+
+    const locales = [...e.target.parentNode.getElementsByTagName("div")]
+    locales.forEach((element) => {
+      this.markAsRadioButton(element, element.dataset.locale === this.data.get("locale"))
+    })
+
+    this.client.setLocale(this.data.get("locale"), (data) => {})
   }
 
   // NCALayer API
@@ -94,6 +108,30 @@ export default class extends Controller {
         const alias = extractKeyAlias(data.getResult())
         this.targets.find("key-list").append(new Option(data.getResult(), alias))
         this.data.set("alias", alias)
+      }
+    )
+  }
+
+  getNotBefore() {
+    this.client.getNotBefore(
+      this.data.get("storage"),
+      this.targets.find("path").value,
+      this.data.get("alias"),
+      this.targets.find("password").value,
+      (data) => {
+        this.targets.find("not-before").value = data.getResult()
+      }
+    )
+  }
+
+  getNotAfter() {
+    this.client.getNotAfter(
+      this.data.get("storage"),
+      this.targets.find("path").value,
+      this.data.get("alias"),
+      this.targets.find("password").value,
+      (data) => {
+        this.targets.find("not-after").value = data.getResult()
       }
     )
   }
